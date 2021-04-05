dcf90a2cfcac3f2d5858c70bfb65e322a63ab263
Kashif Ali <Kashif.ali@carbonteq.com>
2021-04-05 22:55:15 +0500

    added gender field

--- src/app/components/userInformationForm/personalInformation/PersonalInformation.jsx
+++ src/app/components/userInformationForm/personalInformation/PersonalInformation.jsx
@@ -1,4 +1,4 @@
-import { Form, Row, Col, DatePicker } from "antd";
+import { Form, Row, Col, DatePicker, Select } from "antd";
 import React from "react";
 import InputTextFieldList from "../../common/inputTextFieldList/InputTextFieldList.jsx";
 const personalInfoInput = [
@@ -26,13 +26,13 @@ const personalInfoInput = [
     ],
   },
 ];
-
+const { Option } = Select;
 export default function PersonalInformation() {
   return (
     <>
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
         <InputTextFieldList inputAttributes={personalInfoInput} />
-        <Col className="gutter-row" span={6} xs={24} xl={8}>
+        <Col className="gutter-row" span={6} xs={12} xl={8}>
           <Form.Item
             label="Date of Birth"
             name={["personalInfo", "dob"]}
@@ -41,6 +41,21 @@ export default function PersonalInformation() {
             ]}
           >
             <DatePicker />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={6} xs={12} xl={8}>
+          <Form.Item
+            label="Gender"
+            name={["personalInfo", "gender"]}
+            rules={[
+              { required: true, message: "Please select your gender!" },
+            ]}
+          >
+            <Select style={{ width: 130 }}>
+              <Option value="None">None</Option>
+              <Option value="Male">Male</Option>
+              <Option value="Female">Female</Option>
+            </Select>
           </Form.Item>
         </Col>
       </Row>

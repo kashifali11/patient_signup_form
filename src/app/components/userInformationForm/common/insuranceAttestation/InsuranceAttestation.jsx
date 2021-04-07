df933cd6a05eba9967282be512f944f645aa11f1
Kashif Ali <Kashif.ali@carbonteq.com>
2021-04-07 22:34:29 +0500

    moved InsuranceAttestation to common folder

---
+++ src/app/components/userInformationForm/common/insuranceAttestation/InsuranceAttestation.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Col, Form, Checkbox } from "antd";
+export default function InsuranceAttestaion() {
+  return (
+    <>
+      <Col span={24} xs={24} sm={12}>
+        <Form.Item
+          name={["insuranceDetails", "attestaion"]}
+          valuePropName="checked"
+          rules={[
+            {
+              required: true,
+              message: "Please click checkbox for attestation!",
+            },
+          ]}
+          wrapperCol={{offset:2}}
+        >
+          <Checkbox>Insurance Attestation</Checkbox>
+        </Form.Item>
+      </Col>
+    </>
+  );
+}
